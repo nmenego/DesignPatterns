7f456c6b670c6a0eac5920c552682eaa95aeb73f
Nico Martin Enego <nmenego@gmail.com>
2014-06-11 14:09:14 +0800

    activity for decorator pattern

---
+++ DecoratorPattern/src/list/builder/AbstractListStringBuilder.java
@@ -0,0 +1,6 @@
+package list.builder;
+
+public abstract class AbstractListStringBuilder {
+	public abstract ListStringBuilder append(String str);
+	public abstract String toString();
+}
---
+++ DecoratorPattern/src/list/builder/ListStringBuilderTest.java
@@ -0,0 +1,15 @@
+package list.builder;
+
+public class ListStringBuilderTest {
+
+	public static void main(String[] args) {
+		// TODO uncomment this after ListStringBuilder has been implemented
+//		ListStringBuilder builder = new ListStringBuilder();
+//		builder.append("DEV-H");
+//		builder.append("DEV-G");
+//		builder.append("DEV-5");
+//		builder.append("DEV-3");
+//		System.out.println(builder.toString());
+	}
+
+}
